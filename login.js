9e69d3eee2ec4a13581478d93015b32909d20b45
Daniel <danielmr@uninorte.edu.co>
2022-11-18 10:49:29 -0500

    Se agrego el proyecto de flutter con su respectivo gitignore

--- login.js
+++
@@ -1,9 +0,0 @@
-function showLoginWindow(){
-    var login = document.getElementById("login-popup")
-    login.classList.remove("hidden")
-}
-
-function hideLoginWindow(){
-    var login = document.getElementById("login-popup")
-    login.classList.add("hidden")
-}
