c43f3367155304e6b636323d27396d4f2b97eb1a
JParavel <jgamezc@uninorte.edu.co>
2022-11-27 22:18:23 -0500

    Docker File

--- Ullet API/app.js
+++ Ullet API/app.js
@@ -4,13 +4,13 @@ import router from "./routes/userRouter.js";
 
 const app = express();
 
-const port = 3000;
+const port = 8080;
 const user = "ullet";
 const password = "ullet";
 const host = "ulletcluster.0o8r4qw.mongodb.net";
 const database = "ullet";
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log("Server running on port "+ port);
 })
 
